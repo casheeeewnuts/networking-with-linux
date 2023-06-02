671001d95573963c40f71b5d68299a77aa38d24b
casheeeewnuts <shu.nonaka+tech@casheeeewnuts.net>
2023-06-02 14:46:38 +0900

    wip

--- lib/ethernet-echo-client/src/main.rs
+++ lib/ethernet-echo-client/src/main.rs
@@ -1,20 +1,17 @@
-use std::num::ParseIntError;
 use pnet::datalink;
 use pnet::datalink::Channel::Ethernet;
 use pnet::datalink::{MacAddr, NetworkInterface};
+use pnet::packet::ethernet::{EtherType, MutableEthernetPacket};
 use pnet::packet::Packet;
-use pnet::packet::ethernet::{MutableEthernetPacket, EtherType};
+use std::num::ParseIntError;
 
 fn main() -> std::io::Result<()> {
     let interface_name = std::env::args().nth(1).unwrap();
-    let dest_addr = {
-        let raw_addr = std::env::args().nth(2).unwrap();
-        let segments = raw_addr.split(":").map(|segment| u8::from_str_radix(segment, 16)).collect::<Result<Vec<u8>, ParseIntError>>().expect("invalid MAC-address format!");
+    let dest_addr =
+        parse_mac_addr(&std::env::args().nth(2).unwrap()).expect("Invalid mac-address!");
 
-        MacAddr(segments[0], segments[1], segments[2], segments[3], segments[4], segments[5])
-    };
-
-    let target_interface = datalink::interfaces().into_iter()
+    let target_interface = datalink::interfaces()
+        .into_iter()
         .filter(|iface: &NetworkInterface| iface.name == interface_name)
         .next()
         .unwrap();
@@ -22,7 +19,7 @@ fn main() -> std::io::Result<()> {
     let (mut tx, _) = match datalink::channel(&target_interface, Default::default()) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
         Ok(_) => panic!("could not listen the interface: {}", interface_name),
-        Err(e) => panic!("{}", e)
+        Err(e) => panic!("{}", e),
     };
 
     let mut packet = [0u8; 14];
@@ -37,4 +34,21 @@ fn main() -> std::io::Result<()> {
     };
 
     tx.send_to(packet.packet(), Some(target_interface)).unwrap()
+}
+
+fn parse_and_into_mac_addr(addr: &str) -> Result<MacAddr, String> {
+    addr.split(":")
+        .map(|segment| u8::from_str_radix(segment, 16))
+        .collect::<Result<Vec<u8>, ParseIntError>>()
+        .map_err(|e| e.to_string())
+        .map(|segments| {
+            MacAddr(
+                segments[0],
+                segments[1],
+                segments[2],
+                segments[3],
+                segments[4],
+                segments[5],
+            )
+        })
 }
--- lib/ethernet-echo-protocol/src/lib.rs
+++ lib/ethernet-echo-protocol/src/lib.rs
@@ -1,8 +1,10 @@
 use pnet::packet::PrimitiveValues;
 use pnet_macros::{packet};
+use pnet_macros;
+use pnet_macros_support::packet::Packet;
 
 #[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct MessageType(pub u16);
+pub struct MessageType(pub u8);
 
 #[allow(non_snake_case)]
 #[allow(non_upper_case_globals)]
@@ -17,9 +19,9 @@ pub mod MessageTypes {
 /// 0                   1                   2                   3
 /// 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-/// | message_type |   check_sum   |           padding            |
+/// | message_type |                     padding                  |
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-/// |                                                             |
+/// |                           payload                           |
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 /// |                                                             |
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -27,31 +29,40 @@ pub mod MessageTypes {
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 #[packet]
 pub struct EthernetEchoProtocol {
-    #[construct_with(u16)]
+    #[construct_with(u8)]
     pub message_type: MessageType,
+
+    #[length = "3"]
+    pub padding: Vec<u8>,
 
     #[payload]
     pub payload: Vec<u8>
 }
 
 impl MessageType {
-    pub fn new(val: u16) -> MessageType {
+    pub fn new(val: u8) -> MessageType {
         MessageType(val)
     }
 }
 
 impl PrimitiveValues for MessageType {
-    type T = (u16,);
-    fn to_primitive_values(&self) -> (u16,) {
+    type T = (u8,);
+
+    #[inline]
+    fn to_primitive_values(&self) -> (u8,) {
         (self.0,)
     }
 }
 
 #[test]
 fn ethernet_echo_protocol_packet_header_test() {
-    let mut packet = [0u8; 14];
+    let mut packet = [0u8; 4 + 6];
     let mut eep_packet = MutableEthernetEchoProtocolPacket::new(&mut packet[..]).unwrap();
 
-    eep_packet.set_message_type(MessageTypes::Request);
-    assert_eq!(eep_packet.get_message_type(), MessageTypes::Request);
+    eep_packet.set_message_type(MessageTypes::Response);
+    assert_eq!(eep_packet.get_message_type(), MessageTypes::Response);
+
+    eep_packet.set_payload("hello!".as_bytes());
+    dbg!(std::string::String::from_utf8(eep_packet.payload().to_vec()).unwrap());
+    dbg!(packet);
 }
