fec8f2215a37a15af0633cb4bee3be0ee2355b2e
casheeeewnuts <shu.nonaka+tech@casheeeewnuts.net>
2023-06-02 14:46:09 +0900

    fix packet handling

--- lib/ethernet-echo-server/src/main.rs
+++ lib/ethernet-echo-server/src/main.rs
@@ -1,7 +1,7 @@
 use pnet::datalink::{self, NetworkInterface};
 use pnet::datalink::Channel::Ethernet;
 use pnet::packet::{Packet, MutablePacket};
-use pnet::packet::ethernet::{EthernetPacket, MutableEthernetPacket};
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket};
 use std::env;
 
 fn main() {
@@ -12,6 +12,7 @@ fn main() {
         .filter(|iface: &NetworkInterface| iface.name == interface_name)
         .next()
         .unwrap();
+    let target_interface_mac_addr = target_interface.mac.unwrap();
 
     let (mut tx, mut rx) = match datalink::channel(&target_interface, Default::default()) {
         Ok(Ethernet(tx, rx)) => (tx, rx),
@@ -26,16 +27,25 @@ fn main() {
 
                 let packet = EthernetPacket::new(packet).unwrap();
 
-                tx.build_and_send(1, packet.packet().len(),
-                    &mut |new_packet| {
-                        let mut new_packet = MutableEthernetPacket::new(new_packet).unwrap();
+                if packet.get_destination() != target_interface_mac_addr {
+                    continue;
+                }
 
-                        new_packet.clone_from(&packet);
+                println!("{} ---> {}: type={}", packet.get_source(), packet.get_destination(), packet.get_ethertype());
+                // dbg!(packet);
 
-                        new_packet.set_source(packet.get_destination());
-                        new_packet.set_destination(packet.get_source());
-                    }
-                );
+                if packet.get_ethertype() != EtherTypes::Ipv4 {
+                    tx.build_and_send(1, packet.packet().len(),
+                                      &mut |new_packet| {
+                                          let mut new_packet = MutableEthernetPacket::new(new_packet).unwrap();
+
+                                          new_packet.clone_from(&packet);
+
+                                          new_packet.set_source(packet.get_destination());
+                                          new_packet.set_destination(packet.get_source());
+                                      }
+                    );
+                }
             },
             Err(e) => {
                 panic!("{}", e);
